2277ed1a839adcc3373db950a59b4bc395c5a6f7
Andrew Jones <drjones@redhat.com>
2020-10-07 11:53:30 +0200

    lib/string: Weaken memset and memcpy
    When linking with other libraries (such as libefi) there's a chance
    these two basic functions will exist there too. Weaken ours to
    avoid linking problems.
    
    Signed-off-by: Andrew Jones <drjones@redhat.com>

--- lib/string.c
+++ lib/string.c
@@ -100,7 +100,7 @@ char *strstr(const char *s1, const char *s2)
     return NULL;
 }
 
-void *memset(void *s, int c, size_t n)
+void __attribute__((__weak__)) *memset(void *s, int c, size_t n)
 {
     size_t i;
     char *a = s;
@@ -111,7 +111,7 @@ void *memset(void *s, int c, size_t n)
     return s;
 }
 
-void *memcpy(void *dest, const void *src, size_t n)
+void __attribute__((__weak__)) *memcpy(void *dest, const void *src, size_t n)
 {
     size_t i;
     char *a = dest;
